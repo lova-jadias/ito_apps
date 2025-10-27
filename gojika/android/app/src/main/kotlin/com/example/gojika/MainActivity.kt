6608197c4f02c8ba356d0b7faf024ea2ffe6ded4
Lova <lova4trav@gmail.com>
2025-10-28 00:09:16 +0300

    Phase 0.1: Initial monorepo structure and Supabase link

---
+++ gojika/android/app/src/main/kotlin/com/example/gojika/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gojika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ rejistra/android/app/src/main/kotlin/com/example/rejistra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rejistra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
